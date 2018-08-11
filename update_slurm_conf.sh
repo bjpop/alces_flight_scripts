b963ff1f7d6c7eff41b61b6df7471136631d70fd
Bernie Pope <bjpope@unimelb.edu.au>
2018-08-11 21:18:09 +1000

    Use sed to edit slurm conf

--- update_slurm_conf.sh
+++ update_slurm_conf.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -l
+#!/bin/bash
 
 #systemctl stop clusterware-slurm-slurmd.service
 sudo cp /home/alces/code/alces_flight_spank_private_tmp/plugstack.conf /opt/clusterware/opt/slurm/etc/plugstack.conf
 sudo cp /home/alces/code/alces_flight_spank_private_tmp/private-tmpdir.so /opt/clusterware/opt/slurm/lib/private-tmpdir.so
 sudo cp /opt/clusterware/opt/slurm/etc/slurm.conf /tmp/slurm.conf.bak
-sudo cp /home/alces/config/alces_flight_slurm_config/slurm.conf /opt/clusterware/opt/slurm/etc/slurm.conf 
+#sudo cp /home/alces/config/alces_flight_slurm_config/slurm.conf /opt/clusterware/opt/slurm/etc/slurm.conf 
+sudo sed -i '/#Epilog=/c\Epilog=/opt/clusterware/opt/slurm/bin/epilog.sh' /opt/clusterware/opt/slurm/etc/slurm.conf 
 sudo cp /home/alces/config/alces_flight_slurm_config/epilog.sh /opt/clusterware/opt/slurm/bin/epilog.sh
 sudo chmod a+x /opt/clusterware/opt/slurm/bin/epilog.sh
 #systemctl start clusterware-slurm-slurmd.service 
