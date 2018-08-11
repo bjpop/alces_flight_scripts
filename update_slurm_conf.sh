76dcb19b368ab241aef55173fbc15454586c6380
Bernie Pope <bjpope@unimelb.edu.au>
2018-08-11 09:32:42 +0000

    Remove slurmd stop/start

--- update_slurm_conf.sh
+++ update_slurm_conf.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -l
-systemctl stop clusterware-slurm-slurmd.service
-cp /home/alces/code/spank-private-tmp/plugstack.conf /opt/clusterware/opt/slurm/etc/plugstack.conf
-cp /home/alces/code/spank-private-tmp/private-tmpdir.so /opt/clusterware/opt/slurm/lib/private-tmpdir.so
-cp /opt/clusterware/opt/slurm/etc/slurm.conf /tmp/slurm.conf.bak
-cp /home/alces/config/slurm.conf /opt/clusterware/opt/slurm/etc/slurm.conf 
-cp /home/alces/config/epilog.sh /opt/clusterware/opt/slurm/bin/epilog.sh
-chmod a+x /home/alces/config/epilog.sh /opt/clusterware/opt/slurm/bin/epilog.sh
-systemctl start clusterware-slurm-slurmd.service 
+
+#systemctl stop clusterware-slurm-slurmd.service
+sudo cp /home/alces/code/alces_flight_spank_private_tmp/plugstack.conf /opt/clusterware/opt/slurm/etc/plugstack.conf
+sudo cp /home/alces/code/alces_flight_spank_private_tmp/private-tmpdir.so /opt/clusterware/opt/slurm/lib/private-tmpdir.so
+sudo cp /opt/clusterware/opt/slurm/etc/slurm.conf /tmp/slurm.conf.bak
+sudo cp /home/alces/config/alces_flight_slurm_config/slurm.conf /opt/clusterware/opt/slurm/etc/slurm.conf 
+sudo cp /home/alces/config/alces_flight_slurm_config/epilog.sh /opt/clusterware/opt/slurm/bin/epilog.sh
+sudo chmod a+x /opt/clusterware/opt/slurm/bin/epilog.sh
+#systemctl start clusterware-slurm-slurmd.service 
